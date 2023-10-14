04ff40e215d99e668c32740de376cde130bcb962
DongHun Park <68440583+Laeti-Park@users.noreply.github.com>
2023-10-14 15:17:37 +0900

    ⭐️Style(blossom/brawlers): 코드 수정

--- src/pages/blossom/brawlers/brawlers_table.tsx
+++ src/pages/blossom/brawlers/brawlers_table.tsx
@@ -1,83 +1,89 @@
-import React, { useEffect, useState } from 'react';
+  import React, { useEffect, useState } from 'react';
 
-import TableHead from '~/components/table/members/head';
-import TableBody from '~/components/table/members/body';
-import Pagination from '~/components/pagination/pagination';
-import SearchMembers from '~/components/search/search_members';
+  import TableHead from '~/components/table/members/head';
+  import TableBody from '~/components/table/members/body';
+  import Pagination from '~/components/pagination/pagination';
+  import SearchMembers from '~/components/search/search_members';
 
-import styles from './brawlers_table.module.scss';
-import BrawlerStats from '~/components/brawlers/brawler_stats';
-import BrawlerSelection from '~/components/brawlers/brawler_selection';
-import BrawlerService from '~/services/brawler_service';
+  import BrawlerStats from '~/components/brawlers/brawler_stats';
+  import BrawlerSelection from '~/components/brawlers/brawler_selection';
+  import BrawlerService from '~/services/brawler_service';
 
-const BrawlersTable = () => {
-  const [brawlers, setBrawlers] = useState([]);
-  const [brawlerStats, setBrawlerStats] = useState([]);
-  const [brawler, setBrawler] = useState({
-    brawlerID: '16000000',
-    name: '쉘리',
-    rarity: '기본',
-    role: '대미지 딜러',
-    gender: '여성',
-  });
+  import styles from './brawlers_table.module.scss';
 
-  const [members, setMembers] = useState([]);
-  const [filterMembers, setFilterMembers] = useState([]);
-  const [page, setPage] = useState(1);
-  const offset = (page - 1) * 10;
-  const total = Math.ceil(filterMembers.length / 10);
-
-  useEffect(() => {
-    BrawlerService.getBrawlers().then((data) => {
-      setBrawlers(data.brawlers);
-      setBrawlerStats(data.stats);
+  const BrawlersTable = () => {
+    const [brawlers, setBrawlers] = useState([]);
+    const [brawlerStats, setBrawlerStats] = useState([]);
+    const [brawler, setBrawler] = useState({
+      brawlerID: '16000000',
+      name: '쉘리',
+      rarity: '기본',
+      role: '대미지 딜러',
+      gender: '여성',
     });
-    BrawlerService.getBlossomMember({
-      brawlerID: brawler.brawlerID,
-    }).then((data) => {
-      setMembers(data);
-    });
-  }, []);
 
-  useEffect(() => {
-    BrawlerService.getBlossomMember({
-      brawlerID: brawler.brawlerID,
-    }).then((data) => {
-      setMembers(data);
-    });
-  }, [brawler.brawlerID]);
+    const [members, setMembers] = useState([]);
+    const [filterMembers, setFilterMembers] = useState([]);
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * 10;
+    const total = Math.ceil(filterMembers.length / 10);
 
-  return (
-    <div className={styles.app}>
-      <BrawlerSelection brawlers={brawlers} setBrawler={setBrawler} />
-      <div>
-        <div className={styles.filter}>
-          <Pagination page={page} total={total} setPage={setPage} />
-          <SearchMembers members={members} setFilterMembers={setFilterMembers} />
-        </div>
-        <BrawlerStats brawler={brawler} brawlerStats={brawlerStats} />
-        <div className={styles.table}>
-          <TableHead col={'닉네임'} colArray={['현재', '최고']} />
-          <div className={styles.body}>
-            {filterMembers.slice(offset, offset + 10).map((member) => {
-              return (
-                <TableBody
-                  key={`${member.userID}_${member.brawlerID}`}
-                  col={member.name}
-                  colArray={[
-                    `${member.currentTrophies}개`,
-                    `${member.highestTrophies}개`,
-                  ]}
-                  colImage={[null, null]}
-                  id={member.userID}
-                />
-              );
-            })}
+    useEffect(() => {
+      BrawlerService.getBrawlers().then((data) => {
+        setBrawlers(data.brawlers);
+        setBrawlerStats(data.stats);
+      });
+      BrawlerService.getBlossomMember({
+        brawlerID: brawler.brawlerID,
+      }).then((data) => {
+        setMembers(data);
+      });
+    }, []);
+
+    useEffect(() => {
+      BrawlerService.getBlossomMember({
+        brawlerID: brawler.brawlerID,
+      }).then((data) => {
+        setMembers(data);
+      });
+    }, [brawler.brawlerID]);
+
+    return (
+      members && (
+        <div className={styles.app}>
+          <BrawlerSelection brawlers={brawlers} setBrawler={setBrawler} />
+          <div>
+            <div className={styles.filter}>
+              <Pagination page={page} total={total} setPage={setPage} />
+              <SearchMembers
+                members={members}
+                setFilterMembers={setFilterMembers}
+              />
+            </div>
+            <BrawlerStats brawler={brawler} brawlerStats={brawlerStats} />
+            <div className={styles.table}>
+              <TableHead col={'닉네임'} colArray={['현재', '최고']} />
+              <div className={styles.body}>
+                {filterMembers.slice(offset, offset + 10).map((member) => {
+                  return (
+                    <TableBody
+                      key={`${member.userID}_${member.brawlerID}`}
+                      col={member.name}
+                      colArray={[
+                        `${member.currentTrophies}개`,
+                        `${member.highestTrophies}개`,
+                      ]}
+                      colImage={[null, null]}
+                      id={member.userID}
+                    />
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
+      )
+    );
+  };
 
-export default BrawlersTable;
+  export default BrawlersTable;
